000d18051fd625acbc7fb4f5e34725c05e3ff05e
IMTIAZ <126008482+IMTIAZ-X@users.noreply.github.com>
2025-07-25 12:37:24 +0600

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.imtiaz.ktimazstudio"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.imtiaz.ktimazstudio"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
